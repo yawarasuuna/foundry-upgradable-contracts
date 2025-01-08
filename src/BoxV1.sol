d9dbeefe72703c8871af4e91c2ec9ef3132e8fee
yawarasuuna <yawarasuuna@proton.me>
2025-01-07 22:55:14 -0300

    feature/add-uups-contracts

---
+++ src/BoxV1.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV1 is Ownable, UUPSUpgradeable {
+    error BoxV1__UnauthorizedOwner();
+
+    uint256 internal number;
+
+    constructor() Ownable(msg.sender) {}
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function version() external pure returns (uint256) {
+        return 1;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
